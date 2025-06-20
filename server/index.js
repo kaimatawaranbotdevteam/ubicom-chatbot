213461e0156f57c584b929053bcd6e8d35e3186c
kaimatawaranbotdevteam <kai.matawaran@BotDevTeam.onmicrosoft.com>
2025-06-20 10:39:30 +0800

    updated port setting

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.get('*', (req, res) => {
 // ---------------
 // Start server
 // ---------------
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
